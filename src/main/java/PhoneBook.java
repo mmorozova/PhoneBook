ceb1351ae4fc3549a72ad7fb01cc48055f286b15
mmorozova <morozovamaria@list.ru>
2026-06-08 15:15:23 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    private Map<String, String> nameToNumber = new HashMap<>();
+
     public int  add(String name, String phoneNumber){
-        return 0;
+        if (!nameToNumber.containsKey(name)) {
+            nameToNumber.put(name, phoneNumber);
+        }
+        return nameToNumber.size();
     }
 }
