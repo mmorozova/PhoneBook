361e5cbbb36b5baf7ff1985bd19567dafa8f4825
mmorozova <morozovamaria@list.ru>
2026-06-08 14:50:23 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class PhoneBookTest {
+
+    @Test
+    public void test_add() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Mary", "2466911"));
+    }
 }
